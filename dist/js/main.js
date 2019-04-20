158ac5292253a3d8c6143490de9f559c0606f940
edwwn <40034470+edwwn@users.noreply.github.com>
2019-04-20 14:56:01 +0300

    up to lines rotate

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+// select dom items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".btn-nav");
+const menuBranding = document.querySelector(".btn-branding");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
